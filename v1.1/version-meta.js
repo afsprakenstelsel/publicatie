1224211055e77cc4cb7efdbc32d5172cb1ae070b
René Hietkamp <rene@hietkamp.nl>
2026-06-17 07:58:37 +0200

    Update documentation

---
+++ v1.1/version-meta.js
@@ -0,0 +1,50 @@
+window.__MEDMIJ_VERSION_META__ = {
+  "current": "v1.1",
+  "current_label": "Versie 1.1",
+  "current_type": "release",
+  "current_path": "/v1.1/",
+  "latest_release": "v1.1",
+  "versions": [
+    {
+      "id": "main",
+      "label": "main (ontwikkeling)",
+      "type": "branch",
+      "path": "/",
+      "date": null
+    },
+    {
+      "id": "feature/account-beveiliging",
+      "label": "Account beveiliging",
+      "type": "branch",
+      "path": "/feature-account-beveiliging/",
+      "date": null
+    },
+    {
+      "id": "feature/aanbiederslijst-uitbreiding",
+      "label": "Aanbiederslijst uitbreiding",
+      "type": "branch",
+      "path": "/feature-aanbiederslijst/",
+      "date": null
+    },
+    {
+      "id": "v1.1",
+      "label": "Versie 1.1",
+      "type": "release",
+      "path": "/v1.1/",
+      "date": "2025-06-17"
+    },
+    {
+      "id": "v1.0",
+      "label": "Versie 1.0",
+      "type": "release",
+      "path": "/v1.0/",
+      "date": "2025-01-01"
+    }
+  ],
+  "changes": {
+    "core.dossier.100": "modified",
+    "core.dossier.101": "modified",
+    "core.dossier.102": "modified"
+  },
+  "has_changes": true
+};
